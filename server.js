d22f4c02f5d838dd07ea915a97434f191ea6f936
emrekabadayi7 <emrekabadayi7@gmail.com>
2023-07-07 18:42:11 -0500

    app.use req res next

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 console.log("bunu gorebiliyor musun?")
 
 // MongoDB Connection
