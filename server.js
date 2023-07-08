713ea0cfb301fba291b675a899b03caa7ff59a8f
emrekabadayi7 <emrekabadayi7@gmail.com>
2023-07-07 20:48:47 -0500

    sadece yerlerini degistirdim cors ile bodyparser urlencoded'ın

--- server.js
+++ server.js
@@ -11,15 +11,14 @@ const port = 3001;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 console.log("bunu gorebiliyor musun?")
 
