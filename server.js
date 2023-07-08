ed66a273d2453d51cdc52209f97337d7ec68317c
emrekabadayi7 <86978697+emrekabadayi7@users.noreply.github.com>
2023-07-07 21:48:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 console.log("bunu gorebiliyor musun?")
 
